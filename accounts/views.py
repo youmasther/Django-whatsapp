304e8fa314ee78d15f979e9eef15810ef55f376c
youmasther <78354169+youmasther@users.noreply.github.com>
2024-04-21 18:00:29 +0000

    feat: handle login form

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import auth
 
 # Create your views here.
 
 def login(request):
-    return render(request, 'login.html')
+    user = request.user
+    if user and user.is_authenticated:
+        return redirect('chat')
+    if request.POST:
+        email = request.POST.get('email','') if 'email' in request.POST  else None
+        password = request.POST.get('password', '') if 'password' in request.POST  else None
+        user = auth.authenticate(email=email , password=password)
+        if user and user.is_active:
+            auth.login(request, user)
+            return redirect('chat')
+        else:
+            return render(request, 'login.html', {'message': 'Login ou mot de passe incorrect'})
+    else:
+        return render(request, 'login.html', {'message': ''})
 
 def register(request):
     return render(request, 'register.html')
