d3158b5314efe6766ae621b5fb4a6a327cf0b843
youmasther <78354169+youmasther@users.noreply.github.com>
2024-04-21 21:57:41 +0000

    refactor: migrate model Message to accounts

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-
+# from core.models  import Messages
 # Create your models here.
 
 # The `MyAccountManager` class is a custom user manager in Python that provides methods for creating
@@ -70,7 +70,6 @@ class User(AbstractBaseUser):
     prenom = models.CharField(max_length=100, null=True, blank=True)
     nom = models.CharField(max_length=100, null=True, blank=True)
     email = models.EmailField(max_length=65,verbose_name="email", unique=True)
-    # username = models.CharField(max_length=10, null=True, blank=True)
     date_joined = models.DateTimeField(verbose_name='date de creation', auto_now_add=True)
     last_login = models.DateTimeField(verbose_name="dernier connexion", auto_now=True)
     is_admin = models.BooleanField(default=False)
@@ -89,6 +88,10 @@ class User(AbstractBaseUser):
     # USERNAME_FIELD = 'email'
     def __str__(self):
         return '{} {}'.format(self.prenom, self.nom)
+    
+    @property
+    def last_message(self):
+        return Messages.objects.filter(sender=self).last()
 
     def get_profile_image_filename(self):
 	    return str(self.profile_image)[str(self.profile_image).index(f'profile_images/{str(pk)}'):]
@@ -98,3 +101,38 @@ class User(AbstractBaseUser):
 
     def has_module_perms(self, app_label):
 	    return True
+    
+
+def get_media_file(instance, filename):
+    """
+    The function `get_media_file` returns the file path for a media file based on the instance ID and
+    filename provided.
+    
+    :param instance: Instance refers to an instance of a model or class in object-oriented programming.
+    In this context, it likely represents an instance of a media file object or entity that is being
+    processed or handled within the code snippet provided
+    :param filename: The `filename` parameter is a string that represents the name of the file being
+    processed or accessed. It could be the name of an image file, video file, audio file, or any other
+    type of media file
+    :return: The function `get_media_file` is returning a string that represents the file path for a
+    media file. The file path is constructed by joining the elements 'images', the ID of the instance,
+    and the filename using the '/' separator.
+    """
+    return '/'.join(['images', str(instance.id),filename])
+
+
+class Messages(models.Model):
+    sender = models.ForeignKey(User, related_name=("sender"), on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name=("receiver"), on_delete=models.CASCADE)
+    content = models.TextField()
+    media_url = models.ImageField(max_length=255, upload_to=get_media_file, null=True, blank=True, default=None)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    class Meta:
+        ordering = ["-timestamp"]
+        verbose_name = "Messages"
+        verbose_name_plural = "Messages"
+        db_table = "Messages"
+
+    def __str__(self):
+        return f"Message {self.id} - {self.content[:20]}"
--- core/views.py
+++ core/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from accounts.models import User
 # Create your views here.
 
+@login_required(login_url='/')
 def chat(request):
-    return render(request, 'index.html')
+    users = User.objects.exclude(email=request.user.email)
+    context = {
+        "users": users
+    }
+    return render(request, 'index.html', context)
