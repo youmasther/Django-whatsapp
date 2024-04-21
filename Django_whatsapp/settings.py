76859bd08c23e670be1994e333e9e5c1d5b97a1e
youmasther <78354169+youmasther@users.noreply.github.com>
2024-04-21 14:22:31 +0000

    feat: init core app and create index.html

--- Django_whatsapp/settings.py
+++ Django_whatsapp/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     # project custom apps
     'accounts',
+    'core',
 
 ]
 
--- Django_whatsapp/urls.py
+++ Django_whatsapp/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
+    path('chat/', include('core.urls')),
 ]
 # +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
