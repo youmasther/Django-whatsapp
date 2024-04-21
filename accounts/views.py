dcfb0e636a5d74a67585ae8fa1f8029b1aa964fa
youmasther <78354169+youmasther@users.noreply.github.com>
2024-04-21 19:25:42 +0000

    feat: handler user register

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import redirect, render
 from django.contrib import auth
 
+from .forms import UserCreationForm
+from .models import User
+
 # Create your views here.
 
 def login(request):
@@ -20,4 +23,29 @@ def login(request):
         return render(request, 'login.html', {'message': ''})
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'POST':
+        email = request.POST.get('email', '') if 'email' in request.POST  else None
+        password1 = request.POST.get('password', '') if 'password' in request.POST  else None
+        password2 =  request.POST.get('confirm', '') if 'confirm' in request.POST  else None
+        prenom = request.POST.get('prenom', '') if 'prenom' in request.POST else None
+        nom = request.POST.get('nom', '') if 'nom' in request.POST else None
+        data = {
+            'email': email,
+            'prenom': prenom,
+            'nom': nom,
+            'password1': password1,
+            'password2': password2,
+        }
+        
+        if User.objects.filter(email=email).first() is not None:
+            return render(request, 'register.html',{"message": "Un utilisateur avec ce email exite déja"})
+
+        user_form = UserCreationForm(data)
+        if user_form.is_valid():
+            print("user created")
+            user_form.save()
+            return redirect('login')
+        else:
+            return render(request, 'register.html',{"message": "Erreur:Veuillez revoir les informations soumisent"})
+    else:
+        return render(request, 'register.html')
