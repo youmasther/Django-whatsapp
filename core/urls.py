234b2714270c6bfaa6a0481a6ecb9d6c6407972f
youmasther <78354169+youmasther@users.noreply.github.com>
2024-04-23 16:07:16 +0000

    feat: handle search bar

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import chat
-from .hx_views import hx_chat, hx_create_message
+from .hx_views import hx_chat, hx_create_message, hx_search
 
 urlpatterns = [
     path('', chat, name="chat"),
     path('hx_chat/<int:id>', hx_chat, name="hx_chat"),
-    path('hx_create_message', hx_create_message, name="hx_create_message")
+    path('hx_create_message', hx_create_message, name="hx_create_message"),
+    path('hx_search', hx_search, name="hx_search"),
 ]
